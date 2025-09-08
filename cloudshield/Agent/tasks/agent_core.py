b5b0b856ff34cbe5fc3c99de627eb307df6be6d7
ACCISA <aniss.chalah@gmail.com>
2025-09-07 23:56:07 -0400

    agent file structure

---
+++ cloudshield/Agent/tasks/agent_core.py
@@ -0,0 +1,19 @@
+import time
+import grpc
+
+
+
+class Agent:
+
+    def __init__(self, agent_id, server_addr):
+        self.tasks = {}
+
+    def register_task(self, task_name, task):
+        pass
+
+    def run_once(self):
+        pass
+
+    def start_core(self):
+        pass
+
