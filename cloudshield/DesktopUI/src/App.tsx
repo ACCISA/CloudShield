efbad414aedfa1b7b9d1f25e81e6806fff5202d8
SoumyBhow <soumybhow1.0@gmail.com>
2026-01-08 18:22:42 -0500

    Remove useless imports

--- cloudshield/DesktopUI/src/App.tsx
+++ cloudshield/DesktopUI/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import LoginPage from "./pages/Login/LoginPage";
 import "./App.css";
 
--- cloudshield/DesktopUI/src/pages/RDPOpvenVPN/RDPOpenVPNCard.tsx
+++ cloudshield/DesktopUI/src/pages/RDPOpvenVPN/RDPOpenVPNCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 declare global {
   interface Window {
     electronAPI?: {
