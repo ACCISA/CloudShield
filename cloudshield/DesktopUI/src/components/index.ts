6d9926a47e7514aefc3113fb2c306afe31a5c1d5
Andrew Harissi Dagher <andrew.h.dagher@gmail.com>
2026-02-17 18:07:20 -0500

    removed desktopui failing changes

--- cloudshield/DesktopUI/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as EmptyState } from "./EmptyState";
-export type { EmptyStateProps } from "./EmptyState";
-
-export { default as Pagination } from "./Pagination";
-export type { PaginationProps } from "./Pagination";
-
-export { default as ProfileImage } from "./ProfileImage";
-export type { ProfileImageProps } from "./ProfileImage";
